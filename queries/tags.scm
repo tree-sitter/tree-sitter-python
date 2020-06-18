80530a8fd3b6407a3e819cec028bd4b2e279c1e9
Timothy Clem <timothy.clem@gmail.com>
2020-06-18 16:39:41 -0700

    Use new prefixes

--- queries/tags.scm
+++ queries/tags.scm
@@ -1,8 +1,8 @@
 (class_definition
- name: (identifier) @name) @class
+ name: (identifier) @name) @definition.class
 
 (function_definition
- name: (identifier) @name) @function
+ name: (identifier) @name) @definition.function
 
 (call
- function: (identifier) @name) @call
+ function: (identifier) @name) @reference.call
