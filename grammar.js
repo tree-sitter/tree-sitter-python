085b127d1f8c2ea3b1eaebe242c0dc83dcdfc1f6
Amaan Qureshi <amaanq12@gmail.com>
2024-11-24 18:08:34 -0500

    feat: use the new reserved rules api

--- grammar.js
+++ grammar.js
@@ -99,6 +99,19 @@ module.exports = grammar({
     $.keyword_identifier,
   ],
 
+  reserved: {
+    global: _ => [
+      // https://docs.python.org/3/reference/lexical_analysis.html#keywords
+      'False', 'await', 'else', 'import', 'pass',
+      'None', 'break', 'except', 'in', 'raise',
+      'True', 'class', 'finally', 'is', 'return',
+      'and', 'continue', 'for', 'lambda', 'try',
+      'as', 'def', 'from', 'nonlocal', 'while',
+      'assert', 'del', 'global', 'not', 'with',
+      'async', 'elif', 'if', 'or', 'yield',
+    ],
+  },
+
   word: $ => $.identifier,
 
   rules: {
